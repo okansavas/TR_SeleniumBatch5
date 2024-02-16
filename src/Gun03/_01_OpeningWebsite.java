79b48f930ceab0cc9dbe1442285bc73f0d75e783
TechnoStudy <ismettechno@gmail.com>
2024-02-16 18:39:53 +0000

    Driver tanımlama ave web sitesi açma

--- src/Gun03/_01_OpeningWebsite.java
+++ src/Gun03/_01_OpeningWebsite.java
@@ -1,8 +1,16 @@
 package Gun03;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class _01_OpeningWebsite {
     public static void main(String[] args) {
 
+        WebDriver driver=new ChromeDriver();  // driver tarayıcı
+                       //new FirefoxDriver();
+                       //new EdgeDriver();
+
+        driver.get("https://techno.study/tr");  // web sayfasını aç
 
     }
 }
