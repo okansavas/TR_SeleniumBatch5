b2f00735aaad7c4ff5cabc17b6cae188e253ad50
TechnoStudy <ismettechno@gmail.com>
2024-02-19 18:44:58 +0000

    _06_FindingByLinkText

---
+++ src/Gun04/_06_FindingByLinkText.java
@@ -0,0 +1,31 @@
+package Gun04;
+
+import Utlity.MyFunc;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _06_FindingByLinkText {
+    // id -> By.id
+    // name -> By.name
+    // class -> By.classname
+    // linktext -> By.linktext (SADECE a tagli elemanlar için kullanılır)
+    // partiallinktext -> By.partiallinktext (SADECE a tagli elemanlar için kullanılır)
+
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.hepsiburada.com/");
+
+        //linktext : linkin görünen yazısı selector olarak kullanılır
+        WebElement siparislerimLinki = driver.findElement(By.linkText("Siparişlerim"));
+        System.out.println("getText() = " + siparislerimLinki.getText());
+
+        //PartialLinktext : Linkin görünen yazısının bir kısmını selector olarak kullanabilirsiniz.
+        WebElement link2=driver.findElement(By.partialLinkText("Süper Fiyat"));
+        System.out.println("link2.getText() = " + link2.getText());
+
+        MyFunc.Bekle(5);
+        driver.quit();
+    }
+}
