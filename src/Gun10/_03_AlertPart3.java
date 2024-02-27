e657991205d2ac3fff36727d8a38151b65a0b4a0
TechnoStudy <ismettechno@gmail.com>
2024-02-27 17:50:45 +0000

    _02_AlertPart2

---
+++ src/Gun10/_03_AlertPart3.java
@@ -0,0 +1,24 @@
+package Gun10;
+
+import Utlity.BaseDriver;
+import Utlity.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class _03_AlertPart3 extends BaseDriver {
+
+    @Test
+    public void Test1() {
+        driver.get("http://demo.seleniumeasy.com/javascript-alert-box-demo.html");
+        MyFunc.Bekle(3);
+
+        WebElement clickMe3 = driver.findElement(By.cssSelector("button[onclick='myConfirmFunction()']"));
+
+
+
+
+    }
+
+
+}
