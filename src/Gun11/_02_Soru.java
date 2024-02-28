a331fffd45831432895a92740e67a0e96bb72599
TechnoStudy <ismettechno@gmail.com>
2024-02-28 17:50:20 +0000

    _03_Soru

--- src/Gun11/_02_Soru.java
+++ src/Gun11/_02_Soru.java
@@ -23,26 +23,28 @@ import java.time.Duration;
 public class _02_Soru extends BaseDriver {
 
     @Test
-    public void Test1(){
+    public void Test1() {
         driver.get("http://demo.seleniumeasy.com/ajax-form-submit-demo.html");
 
-        WebElement title=driver.findElement(By.id("title"));
+        WebElement title = driver.findElement(By.id("title"));
         title.sendKeys("müşteri memnuniyeti");
 
-        WebElement description=driver.findElement(By.id("description"));
+        WebElement description = driver.findElement(By.id("description"));
         description.sendKeys("Yeni aldığım ürün oldukça oldukça iyi çıktı, teşekkür ederiz.");
 
-        WebElement submit=driver.findElement(By.id("btn-submit"));
+
+        WebElement submit = driver.findElement(By.id("btn-submit"));
         submit.click();
 
-       wait.until(ExpectedConditions.textToBe(By.id("submit-control"), "Form submited Successfully!"));
-       WebElement mesaj=driver.findElement(By.id("submit-control"));
+        //WebElement msg= driver.findElement(By.xpath("//*[text()='Form submited Successfully!']"));
 
-        Assert.assertTrue("Aranılan mesaj bulunamadı",  mesaj.getText().equals("Form submited Successfully!") );
+        wait.until(ExpectedConditions.textToBe(By.id("submit-control"), "Form submited Successfully!"));
+        WebElement mesaj = driver.findElement(By.id("submit-control"));
+
+        Assert.assertTrue("Aranılan mesaj bulunamadı", mesaj.getText().equals("Form submited Successfully!"));
 
         BekleKapat();
     }
-
 
 
 }
---
+++ src/Gun11/_03_Soru.java
@@ -0,0 +1,13 @@
+package Gun11;
+
+/*
+Senaryo
+1-  https://www.demoblaze.com/index.html  siteyi açınız.
+2- Samsung galaxy s6  linkine tıklayınız.
+3- Sepete ekleyiniz.
+4- Daha Sonra PRODUCT STORE yazısına tıklatarak ana ekrana geri dönününz
+ */
+
+public class _03_Soru {
+
+}
