fbb87363cf167b2e71a4fe08688b734791137172
TechnoStudy <ismettechno@gmail.com>
2024-02-28 18:32:53 +0000

    _04_Soru

---
+++ src/Gun11/_04_Soru.java
@@ -0,0 +1,12 @@
+package Gun11;
+
+/*
+    Senaryo:
+    https://google.com/ da selenium kelimesini aratınız.
+    ilk çıkan sonuca tıklatıp,
+    çıkan URL yi ekrana yazdırınız.
+    Beklenen url https://www.selenium.dev/  olduğunu doğrulayınız
+ */
+
+public class _04_Soru {
+}
