261910b787eadba096e4a57bc2143209c0bc0a49
TechnoStudy <ismettechno@gmail.com>
2024-02-29 18:11:56 +0000

    _03_Soru

--- src/Gun12/_02_Soru.java
+++ src/Gun12/_02_Soru.java
@@ -8,5 +8,35 @@ package Gun12;
  */
 
 
-public class _02_Soru {
+import Utlity.BaseDriver;
+import Utlity.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class _02_Soru extends BaseDriver {
+
+    @Test
+    public void Test1(){
+        driver.get("https://chercher.tech/practice/frames");
+        MyFunc.Bekle(2);
+
+        driver.switchTo().frame(0); // ilk frame geçtim
+        WebElement input=driver.findElement(By.cssSelector("#topic+input"));
+        input.sendKeys("Türkiye");
+
+        driver.switchTo().defaultContent(); // direk ana sayfa
+        //driver.switchTo().parentFrame(); // 1 geri gel 1 adım old. için aynı
+
+        driver.switchTo().frame(1); // 2. frame e geçtim
+
+        WebElement webSelect=driver.findElement(By.id("animals"));
+        Select nesneSelect=new Select(webSelect);
+        nesneSelect.selectByVisibleText("Avatar");
+
+        BekleKapat();
+    }
+
+
 }
